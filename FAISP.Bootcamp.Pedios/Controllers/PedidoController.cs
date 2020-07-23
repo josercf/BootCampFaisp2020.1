41434cf45293259502573ae577ada4d88f5316e2
Unidas <unidas@Mac-mini.local>
2020-07-23 20:14:25 -0300

    criar classes

---
+++ FAISP.Bootcamp.Pedios/Controllers/PedidoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FAISP.Bootcamp.Pedios
+{
+    public class PedidoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index(int idItemCardapio)
+        {
+            return View();
+        }
+    }
+}
---
+++ FAISP.Bootcamp.Pedios/Models/PedidoModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FAISP.Bootcamp.Pedios.Models;
+
+namespace FAISP.Bootcamp.Pedios.Models
+{
+    public class PedidoModel
+    {
+        public PedidoModel()
+        {
+        }
+
+        public string NomeCliente { get; set; }
+
+        public string TelefoneContato { get; set; }
+
+        public string FormaPagamento { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string  Logradouro { get; set; }
+
+        public string Numero{ get; set; }
+
+        public string Cep { get; set; }
+
+        public string Cidade  { get; set; }
+
+        public string Complemento { get; set; }
+
+        public Pizza Pizza { get; set; }
+    }
+}
